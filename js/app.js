1e1878759c629070f6da3fb3826d0281726151cc
Gichelle Amon <amongichelle@gmail.com>
2022-03-05 19:28:22 -0800

    feat(js): get html elements using query selector id

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+const birthdate = document.querySelector('#birthdate');
+const phoneNumber = document.querySelector('#phoneNumber');
+const submitBtn = document.querySelector('#submitBtn');
+
