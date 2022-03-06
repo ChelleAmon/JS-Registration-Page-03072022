3f70db313c4a79b383b49ed6fb2dd38a69e18cd3
Gichelle Amon <amongichelle@gmail.com>
2022-03-05 21:15:58 -0800

    feat(js): create getForm() function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,52 @@
-const firstName = document.querySelector("#firstName");
+const firstName =  document.querySelector("#firstName");
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
 const birthdate = document.querySelector('#birthdate');
 const phoneNumber = document.querySelector('#phoneNumber');
+
 const submitBtn = document.querySelector('#submitBtn');
 
+const fName = document.querySelector(".fName");
+const lName = document.querySelector(".lName");
+const emailAddress = document.querySelector(".emailAddress");
+const bDate = document.querySelector(".bDate");
+const pNumber = document.querySelector(".pNumber");
+
+
+
+submitBtn.addEventListener('click', getForm)
+
+
+function getForm(){
+
+    if(confirm("Are you sure?")){
+
+        fName.textContent = firstName.value
+        lName.textContent = lastName.value
+        emailAddress.textContent = email.value
+        bDate.textContent = birthdate.value
+        pNumber.textContent = phoneNumber.value
+
+
+        fName.innerHTML = fName.textContent.toUpperCase()
+        lName.innerHTML = lName.textContent.toUpperCase()
+        emailAddress.innerHTML = emailAddress.textContent.toLowerCase()
+        bDate.innerHTML = new Date(bDate.textContent).toDateString()
+        pNumber.innerHTML = phoneNumber.textContent
+    
+    }else{
+
+        firstName.value = ""
+        lastName.value = ""
+        email.value = ""
+        birthdate.value.Date = ""
+        phoneNumber.value = ""
+
+    }
+
+    firstName.value = ""
+    lastName.value = ""
+    email.value = ""
+    birthdate.value =  new Date('','','');
+    phoneNumber.value = ""
+}
